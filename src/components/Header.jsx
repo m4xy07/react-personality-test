44531c234958aa636448df41d49b75ae10ab2410
Maximus <hey@m4xy.org>
2024-06-05 00:19:39 +0530

    bettrname

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,8 @@ import { Link } from 'react-router-dom';
 const Header = () => {
   return (
 <header className="bg-blue-500 text-white p-5">
-  <h1 className="text-4xl">Superhero Personality Quiz</h1>
+  <h1 className="text-4xl">Hero Within</h1>
+  <h3 className="text-2xl">Discover Your Super Alter-Ego</h3>
       <nav className="flex justify-center mt-4">
         <Link to="/" className="text-gray-600 hover:text-gray-900 transition duration-300 ease-in-out mx-4">
           Home
